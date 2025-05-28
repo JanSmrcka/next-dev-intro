a4ad4c2833ba7ec0dc49559888db3b33da3118a1
Trong Dat Luu <david19au@gmail.com>
2025-05-28 14:19:27 +0200

    feat: enhance todo functionality with error handling and detail view

--- src/actions/todo-actions.ts
+++ src/actions/todo-actions.ts
@@ -1,15 +1,65 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
 import prisma from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function createTodo(formData: FormData) {
   const todoName = formData.get("todo-text") as string;
   const newTodo = {
     name: todoName,
   };
+
   await prisma.todo.create({
     data: newTodo,
   });
+
+  revalidatePath("/");
+}
+
+export async function toggleTodo(id: number) {
+  if (!id) {
+    throw new Error("Todo ID is required");
+  }
+
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!todo) {
+    throw new Error("Todo not found!");
+  }
+
+  await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      completed: !todo.completed,
+    },
+  });
+  revalidatePath("/");
+  revalidatePath(`/todos/${id}`);
+}
+
+export async function deleteTodo(id: number) {
+  if (!id) {
+    throw new Error("Todo ID is required");
+  }
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!todo) {
+    throw new Error("Todo not found!");
+  }
+  await prisma.todo.delete({
+    where: {
+      id,
+    },
+  });
+
   revalidatePath("/");
 }
--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,22 +1,58 @@
 import { Header } from "@/components/header";
-import { TodosSection } from "@/components/todos/todos-section";
 import prisma from "@/lib/prisma";
+import Link from "next/link";
 
-const fetchTodos = async () => {
-  const response = await prisma.todo.findMany();
-  return response;
-};
+async function getTodo(id: number) {
+  return await prisma.todo.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
 
-export default async function Home() {
-  const todos = await fetchTodos();
+const TodoDetailPage = async ({ params }: { params: { id: string } }) => {
+  // Simulating fetching a todo item based on the ID from params
+  const queryParams = await params;
+  const todo = await getTodo(Number(queryParams.id));
+
+  if (!todo) {
+    return <div>Not found</div>;
+  }
 
   return (
     <>
-      <Header title="My Todo List" subtitle="Add your tasks" />
-      <TodosSection todos={todos} />
-      <footer>
-        <p>Click on a task to mark it as completed</p>
-      </footer>
+      <Header title="Todo Detail" subtitle="Here is detail of todo" />
+      <div className="todo-detail">
+        <div className="todo-detail-card">
+          <h2>{todo.name}</h2>
+          <div className="todo-detail-status">
+            Status:{" "}
+            <span className={todo.completed ? "completed" : "active"}>
+              {todo.completed ? "Completed" : "Active"}
+            </span>
+          </div>
+          <div className="todo-detail-status">
+            Priority: <span className={"completed"}>{todo.priority}</span>
+          </div>
+
+          {todo.description && (
+            <div className="todo-detail-description">
+              <p>{todo.description}</p>
+            </div>
+          )}
+        </div>
+
+        <div>
+          <Link href="/">
+            <button className="back-button">Back to Home</button>
+          </Link>
+          <button className="complete-button">
+            {todo.completed ? "Undo" : "Complete"}
+          </button>
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default TodoDetailPage;
