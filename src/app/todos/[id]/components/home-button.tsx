17b57a5800ba79e905d4dc0b325762faa859d70d
Jan Smrcka <jan.smrck@gmail.com>
2025-05-29 23:37:18 +0200

    use transition in toggle button

--- src/app/todos/[id]/components/home-button.tsx
+++ src/app/todos/[id]/components/home-button.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export const HomeButton = () => {
   const router = useRouter();
-
-  const searchParams = useSearchParams();
-
-  const search = searchParams.get("search");
-
-  console.log(search);
-
   function handleRedirect() {
     router.push("/");
   }
-
   return (
     <button onClick={handleRedirect} className="back-button">
       Back to Home
--- src/app/todos/[id]/components/toggle-button.tsx
+++ src/app/todos/[id]/components/toggle-button.tsx
@@ -1,18 +1,28 @@
 "use client";
 import { toggleTodo } from "@/actions/todo-actions";
 import { Todo } from "@prisma/client";
+import { useTransition } from "react";
 
 interface Props {
   todo: Todo;
 }
 export const ToggleButton = ({ todo }: Props) => {
+  const [pending, startTransition] = useTransition();
+
   function handleToggle() {
-    toggleTodo(todo.id);
+    startTransition(async () => {
+      await toggleTodo(todo.id);
+    });
   }
 
   return (
-    <button onClick={handleToggle} className="complete-button">
+    <button
+      onClick={handleToggle}
+      disabled={pending}
+      className="complete-button"
+    >
       {todo.completed ? "Undo" : "Complete"}
+      {pending && <span>...</span>}
     </button>
   );
 };
