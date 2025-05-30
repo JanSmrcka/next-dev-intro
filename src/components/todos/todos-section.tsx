dd416e10d14259a5f8edc563a9050b35af67e864
Jakub Kafka <info@goliathus.net>
2025-05-30 09:56:25 +0200

    Add filtering functionality to TodosSection component and update styles for filter input

--- src/components/todos/todos-section.tsx
+++ src/components/todos/todos-section.tsx
@@ -1,18 +1,35 @@
+"use client";
 import { Todo } from "@prisma/client";
 import { TodoForm } from "./todo-form";
 import { TodoItem } from "./todo-item";
+import { useState } from "react";
 
 type Props = {
   todos: Todo[];
 };
 
 export const TodosSection = ({ todos }: Props) => {
+  const [filter, setFilter] = useState("");
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <main>
       <TodoForm />
+      <div className="filter-container">
+        <input
+          type="text"
+          placeholder="Filter todos..."
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+          className="filter-input"
+        />
+      </div>
       <div className="todo-container">
         <ul>
-          {todos?.map((todo) => {
+          {filteredTodos?.map((todo) => {
             return <TodoItem key={todo.id} todo={todo} />;
           })}
         </ul>
