436f1e34ce4d3bdd0db0dca952c29feb0d501bd4
Jakub Kafka <info@goliathus.net>
2025-05-30 10:32:21 +0200

    Enhance TodoItem component with clickable list items for navigation; update styles for better interactivity and layout

--- src/components/todos/todo-item.tsx
+++ src/components/todos/todo-item.tsx
@@ -2,20 +2,33 @@
 import { DeleteButton } from "@/app/todos/[id]/components/delete-button";
 import { ToggleButton } from "@/app/todos/[id]/components/toggle-button";
 import { Todo } from "@prisma/client";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 type TodoItemProps = {
   todo: Todo;
 };
+
 export const TodoItem = ({ todo }: TodoItemProps) => {
+  const router = useRouter();
+
+  const handleClick = (e: React.MouseEvent) => {
+    // Pokud byl kliknut na tlačítko, nechceme přesměrovat na detail
+    if ((e.target as HTMLElement).closest("button")) {
+      return;
+    }
+    router.push(`/todos/${todo.id}`);
+  };
+
   return (
-    <li className={todo.completed ? "completed" : ""}>
+    <li
+      className={`${todo.completed ? "completed" : ""} clickable`}
+      onClick={handleClick}
+    >
       <span>{todo.name}</span>
-      <DeleteButton todo={todo} />
-      <ToggleButton todo={todo} />
-      <Link href={`/todos/${todo.id}`} className="link">
-        Go to Detail
-      </Link>
+      <div className="todo-actions">
+        <DeleteButton todo={todo} />
+        <ToggleButton todo={todo} />
+      </div>
     </li>
   );
 };
